6fdad0c4ea653c7b2cf1c548bc15c5181d9d78e1
alex <70163067+rapbattlegod32@users.noreply.github.com>
2025-03-14 19:58:15 +0000

    main

---
+++ main.lua
@@ -0,0 +1,146 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))() 
+
+local Window = Rayfield:CreateWindow({
+   Name = "Westbridge Script",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "giga.ae Hub",
+   LoadingSubtitle = "by giga.ae",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "giga.aeHub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "e3knAyNqvR", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+-- main tab
+local MainTab = Window:CreateTab("🏡 Home", nil) -- Title, Image
+local MainSection = MainTab:CreateSection("Information")
+
+-- money statistic
+local guimoney = game:GetService("Players").LocalPlayer.PlayerGui.GameUI.GameUIContainer.Balance
+local moneylabel = MainTab:CreateLabel(guimoney.Text, "pound-sterling", false) -- Title, Icon, Color, IgnoreTheme
+guimoney:GetPropertyChangedSignal("Text"):Connect(function()
+    moneylabel:Set(guimoney.Text, "pound-sterling", false) -- Title, Icon, Color, IgnoreTheme
+ end)
+
+-- health
+local healthbar = game:GetService("Players").LocalPlayer.PlayerGui.GameUI.GameUIContainer.HealthBar.Bar
+local healthbar_size = math.floor(healthbar.Size.X.Scale * 100) -- Convert to whole number
+local formatted_health = string.format("%03d", healthbar_size) -- Format as 3-digit
+local healthlabel = MainTab:CreateLabel(formatted_health, "heart-pulse", false) -- Title, Icon, Color, IgnoreTheme
+healthbar:GetPropertyChangedSignal("Size"):Connect(function()
+    local healthbar_size = math.floor(healthbar.Size.X.Scale * 100)
+    local formatted_health = string.format("%03d", healthbar_size)
+    healthlabel:Set(formatted_health, "heart-pulse", false)
+end)
+-- hunger
+local hungerbar = game:GetService("Players").LocalPlayer.PlayerGui.GameUI.GameUIContainer.HungerBar.Bar
+local hungerbar_size = math.floor(hungerbar.Size.X.Scale * 100) -- Convert to whole number
+local formatted_hunger = string.format("%03d", hungerbar_size) -- Format as 3-digit
+local hungerlabel = MainTab:CreateLabel(formatted_hunger, "utensils", false) -- Title, Icon, Color, IgnoreTheme
+hungerbar:GetPropertyChangedSignal("Size"):Connect(function()
+    local hungerbar_size = math.floor(hungerbar.Size.X.Scale * 100)
+    local formatted_hunger = string.format("%03d", hungerbar_size)
+    hungerlabel:Set(formatted_hunger, "utensils", false)
+end)
+
+-- team
+local team = game:GetService("Players").LocalPlayer.PlayerGui.GameUI.GameUIContainer.Profile.Team
+local teamlabel = MainTab:CreateLabel(team.Text, "users-round", false)
+team:GetPropertyChangedSignal("Text"):Connect(function()
+    teamlabel:Set(team.Text, "users-round", false) -- Title, Icon, Color, IgnoreTheme
+ end)
+
+local Divider = MainTab:CreateDivider()
+
+-- Get the Players service
+local Players = game:GetService("Players")
+
+-- Initial player count
+local playerCount = #Players:GetPlayers()
+local playercountlabel = MainTab:CreateLabel(tostring(playerCount), "server", false)
+
+-- Function to update player count
+local function updatePlayerCount()
+    local playerCount = #game.Players:GetPlayers()
+    playercountlabel:Set(playerCount, "server", false)
+end
+
+updatePlayerCount()
+game.Players.PlayerAdded:Connect(updatePlayerCount)
+game.Players.PlayerRemoving:Connect(updatePlayerCount)
+
+-- police and admin online
+local Teams = game:GetService("Teams") -- Define the Teams Service
+
+local function updateTeamCounts()
+    local civTeam = Teams["Civilian"]:GetPlayers()
+    local royalMilitaryPoliceTeam = Teams["Royal Military Police"]:GetPlayers()
+    local westbridgeBouroughCouncilTeam = Teams["Westbridge Borough Council"]:GetPlayers()
+    local metropolitanPoliceServiceTeam = Teams["Metropolitan Police Service"]:GetPlayers()
+    local britishArmedForcesTeam = Teams["British Armed Forces"]:GetPlayers()
+    local londonAmbulanceServiceTeam = Teams["London Ambulance Service"]:GetPlayers()
+    local londonFireBrigadeTeam = Teams["London Fire Brigade"]:GetPlayers()
+    local racTeam = Teams["RAC"]:GetPlayers()
+    local aATeam = Teams["Automobile Association"]:GetPlayers()
+    local jackboysTeam = Teams["Jackboys"]:GetPlayers()
+
+    local totalRepairTeam = #jackboysTeam + #aATeam + #racTeam
+    local totalPoliceTeam = #metropolitanPoliceServiceTeam + #royalMilitaryPoliceTeam 
+
+    local teamString = "Civilians: " .. #civTeam ..
+                       " | Police: " .. totalPoliceTeam ..
+                       " | Ambulance: " .. #londonAmbulanceServiceTeam ..
+                       " | Fire: " .. #londonFireBrigadeTeam ..
+                       " | Repair: " .. totalRepairTeam
+
+    teamcountlabel:Set(teamString, "scroll-text", false)
+end
+
+updateTeamCounts()
+game.Players.PlayerAdded:Connect(updateTeamCounts)
+game.Players.PlayerRemoving:Connect(updateTeamCounts)
+
+-- vehicle tab
+local VehicleTab = Window:CreateTab("🚗 Vehicle")
+local VehicleFuel = VehicleTab:CreateSection("⛽ Fuel")
+
+local plus10fuel = VehicleTab:CreateButton({
+    Name = "⚠️ Terminate",
+    Callback = function()
+        Rayfield:Destroy()
+    end,
+ })
+
+-- system tab
+local SystemTab = Window:CreateTab("🖥️ System")
+local SystemControls = SystemTab:CreateSection("⚙️ System Controls")
+
+local Button = SystemTab:CreateButton({
+    Name = "⚠️ Terminate",
+    Callback = function()
+        Rayfield:Destroy()
+    end,
+ })
